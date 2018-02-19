e678bf51cee3be376e5ef416fa5690c7cd9dadac
hongrad <benoit.costechareyre07@gmail.com>
2018-02-19 18:37:45 +0100

    création des routes

--- initServeur.js
+++ initServeur.js
@@ -8,16 +8,55 @@ var EtudiantModule = require('./Classes/Etudiant.js');
 
 var server = express();
 
+server.get('/', function(req, res) {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.sendFile(path.join((__dirname + '/template/accueil.html')))
+});
+server.get('/connexion', function(req, res) {
+  res.sendFile(path.join((__dirname + '/template/connexion.html')))
+});
+server.get('/creerQuestionnaire', function(req, res) {
+  res.sendFile(path.join((__dirname + '/template/creerQuestionnaire.html')))
+});
+
+// Les routes qui suivent seront à modifier, elles me servent à tester mes pages pour l'instant
+server.get('/question', function(req, res) {
+  res.sendFile(path.join((__dirname + '/template/question.html')))
+});
+server.get('/reponse', function(req, res) {
+  res.sendFile(path.join((__dirname + '/template/reponse.html')))
+});
+server.get('/attente', function(req, res) {
+  res.sendFile(path.join((__dirname + '/template/attente.html')))
+});
+server.get('/finQuestion', function(req, res) {
+  res.sendFile(path.join((__dirname + '/template/finQuestion.html')))
+});
+server.get('/listeParticipants', function(req, res) {
+  res.sendFile(path.join((__dirname + '/template/listeParticipants.html')))
+});
+server.get('/questionnaires', function(req, res) {
+  res.sendFile(path.join((__dirname + '/template/questionnaires.html')))
+});
+server.get('/questionnaire', function(req, res) {
+  res.sendFile(path.join((__dirname + '/template/previewQuestionnaire.html')))
+});
+
 server.get('/:idObjet/Questionnaire', function(req, res) {
   var params = {};
   params.idObjet = req.params.idObjet;
   params.nom = "idQuestionnaire";
   res.render('Questionnaire.ejs', params);
 });
-server.get('/', function(req, res) {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.sendFile(path.join((__dirname + '/template/accueil.html')))
-});
+/*
+server.get('/:idObjet/Question', function(req, res) {
+  var params = {};
+  params.idObjet = req.params.idObjet;
+  params.nom = "idQuestionn";
+  res.render('Question.ejs', params);
+});*/
+// Créer une route pour: la reponse à une question, une pour les questionnaires d'un prof,
+// une pour la preview d'un questionnaire, la liste des participants à un questionnaire
 
 var connection = mysql.createConnection({
   host : 'localhost',
